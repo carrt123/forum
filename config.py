504d9b880cdf6fbdf69fcb8d85e444bc79c6711b
carrt123 <sam@globadevops.io>
2025-08-16 03:31:16 +0800

    chore：添加邮箱配置

--- config.py
+++ config.py
@@ -2,13 +2,21 @@ class BaseConfig:
     SECRET_KEY = '123456789'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/forum?charset=utf8mb4"
+
+    MAIL_SERVER = "smtp.qq.com"
+    MAIL_USE_SSL = True
+    MAIL_PORT = 465
+    MAIL_USERNAME = "2368996924@qq.com"
+    MAIL_PASSWORD = "huxyljfxunyuebic"
+    MAIL_DEFAULT_SENDER = "2368996924@qq.com"
+
 
 class TestingConfig(BaseConfig):
     pass
 
+
 class ProductionConfig(BaseConfig):
     pass
-
-
