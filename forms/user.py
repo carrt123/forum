8fd6341d111b5b4b4cee6eedcc9375d232044e3a
carrt123 <sam@globadevops.io>
2025-08-23 22:15:46 +0800

    feat: 添加登入表单

--- forms/user.py
+++ forms/user.py
@@ -1,10 +1,11 @@
-from wtforms import Form, StringField, ValidationError
+from wtforms import Form, StringField, ValidationError, BooleanField
 from wtforms.validators import Email, EqualTo, Length
 from exts import cache
 from models.user import UserModel
+from baseform import BaseForm
 
 
-class RegisterForm(Form):
+class RegisterForm(BaseForm):
     email = StringField(validators=[Email(message="请输入正确格式的邮箱!")])
     captcha = StringField(validators=[Length(min=4, max=4, message="请输入正确格式的验证码!")])
     username = StringField(validators=[Length(min=2, max=20, message="请输入正确长度用户名!")])
@@ -23,3 +24,9 @@ class RegisterForm(Form):
         cache_captcha = cache.get(email)
         if not cache_captcha or captcha != cache_captcha:
             raise ValidationError(message="验证码错误!")
+
+
+class LoginForm(BaseForm):
+    email = StringField(validators=[Email(message="请输入正确格式的邮箱!")])
+    password = StringField(validators=[Length(min=6, max=20, message="请输入正确长度的密码!")])
+    remember = BooleanField()
