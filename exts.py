1e724a9de47cf512ee7194717b431089107f11e1
carrt123 <sam@globadevops.io>
2025-08-23 12:14:20 +0800

    feat: 添加CSRF保护扩展

--- exts.py
+++ exts.py
@@ -2,8 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_caching import Cache
+from flask_wtf import CSRFProtect
 
 db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
 cache = Cache()
+csrf = CSRFProtect()
